464e9758ff938297bb22a45aa1257f784e592b21
RiddleSatan <109842309+RiddleSatan@users.noreply.github.com>
2023-08-27 09:43:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,72 @@
 var timer = 60;
 var hitrn;
-var score=0;
+var score = 0;
 
-function hit(){
- hitrn=Math.floor(Math.random()*10);
-    document.querySelector("#hv").textContent=hitrn;
+function hit() {
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hv").textContent = hitrn;
 }
 
-function bubble(){
-var clutter = "";
-for (var i = 1; i <= 102; i++) {
-  var rn = Math.floor(Math.random() * 10);
-  clutter += `<div class="bubble">${rn}</div>`;
-}
-document.querySelector("#pbtm").innerHTML = clutter;
+function bubble() {
+  var clutter = "";
+  for (var i = 1; i <= 102; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${rn}</div>`;
+  }
+  document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-function time() {   
+function time() {
   var a = setInterval(() => {
     if (timer > 0) {
       timer--;
       document.querySelector("#st").textContent = timer;
     } else {
       clearInterval(a);
-      document.querySelector("#pbtm").innerHTML="<h1 id=`over`>Gamer Over Fuiyooooo!</h1>";
+      document.querySelector("#pbtm").innerHTML =
+        "<h1 id=`over`>Gamer Over Fuiyooooo!</h1>";
     }
   }, 1000);
-
 }
 
-function iscore(){
-    score+=10;
-    document.querySelector("#sc").textContent=score;
+function iscore() {
+  score += 10;
+  document.querySelector("#sc").textContent = score;
 }
 
-document.querySelector("#pbtm").addEventListener("click" , function (data){
-    var rn=(Number(data.target.textContent));
-    if(rn === hitrn){
-        bubble();
-        hit();
-        iscore();
+function dscore() {
+  score -= 5;
+  document.querySelector("#sc").textContent = score;
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (data) {
+  var rn = Number(data.target.textContent);
+  console.log(data.target);
+  if (timer > 0) {
+    if (rn === hitrn) {
+      bubble();
+      hit();
+      iscore();
     }
-   
+  }
 });
- 
 
+function rt() {
+  var a = setInterval(function bubble() {
+    if (timer > 0) {
+      var clutter = "";
+      for (var i = 1; i <= 102; i++) {
+        var rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
+      }
+    } else {
+      clearInterval(a);
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
+  }, 1000);
+}
 
+rt();
 time();
 bubble();
 hit();
